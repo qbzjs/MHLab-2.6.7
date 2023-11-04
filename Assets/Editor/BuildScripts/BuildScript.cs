6b4bf06c5f2e26afd927131ffbb70cbd7b1f5d20
LukeGus <bugattiguy527@gmail.com>
2023-11-03 20:49:18 -0500

    Update

--- Assets/Editor/BuildScripts/BuildScript.cs
+++ Assets/Editor/BuildScripts/BuildScript.cs
@@ -22,11 +22,17 @@ public class BuildScript
     {
         string projectName = "Terron"; // Set the name of your project here
         string outputPath = $"Builds/Windows/{projectName}.exe";
+        
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        
         buildPlayerOptions.scenes = GetEnabledScenes();
         buildPlayerOptions.locationPathName = outputPath;
         buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        
         PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.IL2CPP);
+        
+        EditorUserBuildSettings.development = false;
+
         BuildPipeline.BuildPlayer(buildPlayerOptions);
     }
 
@@ -35,15 +41,19 @@ public class BuildScript
     {
         string projectName = "TerronServer"; // Set the name of your project here
         string outputPath = $"Builds/Linux/{projectName}Server.x86_64";
+        
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        
         buildPlayerOptions.scenes = GetEnabledScenes();
         buildPlayerOptions.locationPathName = outputPath;
         buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
         buildPlayerOptions.subtarget = (int)StandaloneBuildSubtarget.Server;
-        
+
         PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.IL2CPP);
         PlayerSettings.SetScriptingDefineSymbolsForGroup(BuildTargetGroup.Standalone, "DEDICATED_SERVER");
         
+        EditorUserBuildSettings.development = false;
+
         BuildPipeline.BuildPlayer(buildPlayerOptions);
     }
 }
