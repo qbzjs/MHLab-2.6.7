b1686110520682fa7183c79a59ca00b9e662c39b
LukeGus <88517757+LukeGus@users.noreply.github.com>
2023-11-05 01:36:11 -0500

    Update BuildScript.cs

--- Assets/Editor/BuildScripts/BuildScript.cs
+++ Assets/Editor/BuildScripts/BuildScript.cs
@@ -51,8 +51,8 @@ public class BuildScript
         buildPlayerOptions.target = BuildTarget.StandaloneLinux64;
         buildPlayerOptions.subtarget = (int)StandaloneBuildSubtarget.Server;
 
-        PlayerSettings.SetScriptingBackend(NamedBuildTarget.Server, ScriptingImplementation.IL2CPP);
-        PlayerSettings.SetScriptingDefineSymbolsForGroup(NamedBuildTarget.Server, "DEDICATED_SERVER");
+        PlayerSettings.SetScriptingBackend(Build.NamedBuildTarget.Server, ScriptingImplementation.IL2CPP);
+        PlayerSettings.SetScriptingDefineSymbolsForGroup(Build.NamedBuildTarget.Server, "DEDICATED_SERVER");
         
         EditorUserBuildSettings.development = false;
 
