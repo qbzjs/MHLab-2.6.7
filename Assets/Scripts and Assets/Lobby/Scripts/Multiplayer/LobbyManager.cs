ec2772503e66c2c9d5f8cb0ff430fbbb02578a5c
LukeGus <bugattiguy527@gmail.com>
2023-11-11 23:53:12 -0600

    Update LobbyManager.cs

--- Assets/Scripts and Assets/Lobby/Scripts/Multiplayer/LobbyManager.cs
+++ Assets/Scripts and Assets/Lobby/Scripts/Multiplayer/LobbyManager.cs
@@ -17,14 +17,11 @@ using Unity.Services.Relay.Models;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LobbyManager : MonoBehaviour {
-
-
+public class LobbyManager : MonoBehaviour 
+{
     private const string KEY_RELAY_JOIN_CODE = "RelayJoinCode";
 
-
     public static LobbyManager Instance { get; private set; }
-
 
     public event EventHandler OnCreateLobbyStarted;
     public event EventHandler OnCreateLobbyFailed;
@@ -35,9 +32,7 @@ public class LobbyManager : MonoBehaviour {
     public class OnLobbyListChangedEventArgs : EventArgs {
         public List<Lobby> lobbyList;
     }
-
-
-
+    
     private Lobby joinedLobby;
     private float heartbeatTimer;
     private float listLobbiesTimer;
@@ -46,9 +41,6 @@ public class LobbyManager : MonoBehaviour {
     private float autoAllocateTimer = 9999999f;
     private bool alreadyAutoAllocated;
     private static IServerQueryHandler serverQueryHandler; // static so it doesn't get destroyed when this object is destroyed
-    private string backfillTicketId;
-    private float acceptBackfillTicketsTimer;
-    private float acceptBackfillTicketsTimerMax = 1.1f;
     private PayloadAllocation payloadAllocation;
 #endif
 
@@ -70,20 +62,11 @@ public class LobbyManager : MonoBehaviour {
     private void CharacterSelectReady_OnInstanceCreated(object sender, EventArgs e) {
         VoteManager.Instance.OnGameStarting += CharacterSelectReady_OnGameStarting;
     }
-
-    private async void CharacterSelectReady_OnGameStarting(object sender, EventArgs e) {
-#if DEDICATED_SERVER
-        if (backfillTicketId != null) {
-            await MatchmakerService.Instance.DeleteBackfillTicketAsync(backfillTicketId);
-        }
-#endif
     }
     */
 
     private async void MultiplayerManager_OnPlayerDataNetworkListChanged(object sender, EventArgs e) {
 #if DEDICATED_SERVER
-        HandleUpdateBackfillTickets();
-
         if (MultiplayerManager.Instance.HasAvailablePlayerSlots()) {
             await MultiplayService.Instance.ReadyServerForPlayersAsync();
         } else {
@@ -159,9 +142,6 @@ public class LobbyManager : MonoBehaviour {
             Debug.Log("Already auto allocated!");
             return;
         }
-
-        SetupBackfillTickets();
-
         alreadyAutoAllocated = true;
 
         var serverConfig = MultiplayService.Instance.ServerConfig;
@@ -181,50 +161,6 @@ public class LobbyManager : MonoBehaviour {
         Debug.Log("Started Server");
     }
 
-    private async void SetupBackfillTickets() {
-        Debug.Log("SetupBackfillTickets");
-        payloadAllocation = await MultiplayService.Instance.GetPayloadAllocationFromJsonAs<PayloadAllocation>();
-
-        backfillTicketId = payloadAllocation.BackfillTicketId;
-        Debug.Log("backfillTicketId: " + backfillTicketId);
-
-        acceptBackfillTicketsTimer = acceptBackfillTicketsTimerMax;
-    }
-
-    private async void HandleUpdateBackfillTickets() {
-        if (backfillTicketId != null && payloadAllocation != null && MultiplayerManager.Instance.HasAvailablePlayerSlots()) {
-            Debug.Log("HandleUpdateBackfillTickets");
-
-            List<Unity.Services.Matchmaker.Models.Player> playerList = new List<Unity.Services.Matchmaker.Models.Player>();
-
-            foreach (PlayerData playerData in MultiplayerManager.Instance.GetPlayerDataNetworkList()) {
-                playerList.Add(new Unity.Services.Matchmaker.Models.Player(playerData.playerId.ToString()));
-            }
-
-            MatchProperties matchProperties = new MatchProperties(
-                payloadAllocation.MatchProperties.Teams, 
-                playerList, 
-                payloadAllocation.MatchProperties.Region, 
-                payloadAllocation.MatchProperties.BackfillTicketId
-            );
-
-            try {
-                await MatchmakerService.Instance.UpdateBackfillTicketAsync(payloadAllocation.BackfillTicketId,
-                    new BackfillTicket(backfillTicketId, properties: new BackfillTicketProperties(matchProperties))
-                );
-            } catch (MatchmakerServiceException e) {
-                Debug.Log("Error: " + e);
-            }
-        }
-    }
-
-    private async void HandleBackfillTickets() {
-        if (MultiplayerManager.Instance.HasAvailablePlayerSlots()) {
-            BackfillTicket backfillTicket = await MatchmakerService.Instance.ApproveBackfillTicketAsync(backfillTicketId);
-            backfillTicketId = backfillTicket.Id;
-        }
-    }
-
     [Serializable]
     public class PayloadAllocation {
         public Unity.Services.Matchmaker.Models.MatchProperties MatchProperties;
@@ -232,7 +168,6 @@ public class LobbyManager : MonoBehaviour {
         public string QueueName;
         public string PoolName;
         public string EnvironmentId;
-        public string BackfillTicketId;
         public string MatchId;
         public string PoolId;
     }
@@ -254,14 +189,6 @@ public class LobbyManager : MonoBehaviour {
                 serverQueryHandler.CurrentPlayers = (ushort)NetworkManager.Singleton.ConnectedClientsIds.Count;
             }
             serverQueryHandler.UpdateServerCheck();
-        }
-
-        if (backfillTicketId != null) {
-            acceptBackfillTicketsTimer -= Time.deltaTime;
-            if (acceptBackfillTicketsTimer <= 0f) {
-                acceptBackfillTicketsTimer = acceptBackfillTicketsTimerMax;
-                HandleBackfillTickets();
-            }
         }
 #endif
     }
